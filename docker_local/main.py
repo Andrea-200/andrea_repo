4c9018eaf49204a89e0c435b5037e00bc78b53d3
Andrea-200 <andreaconfig713@gmail.com>
2024-07-26 13:01:44 +0530

    v1.1.0.0

---
+++ docker_local/main.py
@@ -0,0 +1,12 @@
+
+from fastapi import FastAPI
+from settings import settings  # Import the settings instance
+
+app = FastAPI()
+
+@app.get("/{variable}")
+def read_variable(variable: str):
+    value = settings.get_attribute(variable)
+    return {variable: value}
+
+
